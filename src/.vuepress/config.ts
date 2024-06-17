80b4ad5186881f5c02c3b73234b51586222a550c
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2024-06-17 19:41:48 +0800

    chore: Update theme options

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,6 @@
 import { getDirname, path } from "@vuepress/utils";
+import { viteBundler } from "@vuepress/bundler-vite";
 import { defineUserConfig } from "vuepress";
-import { redirectPlugin } from "vuepress-plugin-redirect";
-import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 import theme from "./theme.js";
 
 const __dirname = getDirname(import.meta.url);
@@ -12,6 +11,11 @@ export default defineUserConfig({
   },
 
   base: "/",
+
+  bundler: viteBundler({
+    viteOptions: {},
+    vuePluginOptions: {},
+  }),
 
   locales: {
     "/zh/": {
@@ -29,103 +33,5 @@ export default defineUserConfig({
 
   theme,
 
-  plugins: [
-    redirectPlugin({
-      autoLocale: true,
-      localeConfig: {
-        "/zh/": ["zh-CN", "zh-TW", "zh"],
-        "/en/": ["en-US", "en-UK", "en"],
-      },
-    }),
-    docsearchPlugin({
-      appId: "JCDEBX6HWI",
-      apiKey: "1620ca7063137c1125a8f78ff0643f09",
-      indexName: "biomaker-pkuio",
-      locales: {
-        "/zh/": {
-          placeholder: "搜索站点",
-          translations: {
-            button: {
-              buttonText: "搜索站点",
-              buttonAriaLabel: "搜索站点",
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: "清除查询条件",
-                resetButtonAriaLabel: "清除查询条件",
-                cancelButtonText: "取消",
-                cancelButtonAriaLabel: "取消",
-              },
-              startScreen: {
-                recentSearchesTitle: "搜索历史",
-                noRecentSearchesText: "没有搜索历史",
-                saveRecentSearchButtonTitle: "保存至搜索历史",
-                removeRecentSearchButtonTitle: "从搜索历史中移除",
-                favoriteSearchesTitle: "收藏",
-                removeFavoriteSearchButtonTitle: "从收藏中移除",
-              },
-              errorScreen: {
-                titleText: "无法获取结果",
-                helpText: "你可能需要检查你的网络连接",
-              },
-              footer: {
-                selectText: "选择",
-                navigateText: "切换",
-                closeText: "关闭",
-                searchByText: "搜索提供者",
-              },
-              noResultsScreen: {
-                noResultsText: "无法找到相关结果",
-                suggestedQueryText: "你可以尝试查询",
-                reportMissingResultsText: "你认为该查询应该有结果？",
-                reportMissingResultsLinkText: "点击反馈",
-              },
-            },
-          },
-        },
-        "/en/": {
-          placeholder: "Search site",
-          translations: {
-            button: {
-              buttonText: "Search site",
-              buttonAriaLabel: "Search site",
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: "Reset query",
-                resetButtonAriaLabel: "Reset query",
-                cancelButtonText: "Cancel",
-                cancelButtonAriaLabel: "Cancel",
-              },
-              startScreen: {
-                recentSearchesTitle: "Recent searches",
-                noRecentSearchesText: "No recent searches",
-                saveRecentSearchButtonTitle: "Save to recent searches",
-                removeRecentSearchButtonTitle: "Remove from recent searches",
-                favoriteSearchesTitle: "Favorites",
-                removeFavoriteSearchButtonTitle: "Remove from favorites",
-              },
-              errorScreen: {
-                titleText: "Unable to get results",
-                helpText: "You might need to check your network connection",
-              },
-              footer: {
-                selectText: "Select",
-                navigateText: "Navigate",
-                closeText: "Close",
-                searchByText: "Search by",
-              },
-              noResultsScreen: {
-                noResultsText: "No results found",
-                suggestedQueryText: "You can try searching for",
-                reportMissingResultsText:
-                  "Do you think this query should have results?",
-                reportMissingResultsLinkText: "Click here to report it",
-              },
-            },
-          },
-        },
-      },
-    }),
-  ],
+  plugins: [],
 });
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -10,7 +10,9 @@ export default hopeTheme({
     url: "https://biomaker-pku.github.io",
   },
 
-  iconAssets: "iconfont",
+  iconAssets: "iconify",
+
+  iconPrefix: "mdi:",
 
   logo: "/logo.svg",
 
@@ -64,6 +66,97 @@ export default hopeTheme({
       serverURL: "https://waline-comment-gold.vercel.app/",
     },
 
+    docsearch: {
+      appId: "JCDEBX6HWI",
+      apiKey: "1620ca7063137c1125a8f78ff0643f09",
+      indexName: "biomaker-pkuio",
+      locales: {
+        "/zh/": {
+          placeholder: "搜索站点",
+          translations: {
+            button: {
+              buttonText: "搜索站点",
+              buttonAriaLabel: "搜索站点",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+        "/en/": {
+          placeholder: "Search site",
+          translations: {
+            button: {
+              buttonText: "Search site",
+              buttonAriaLabel: "Search site",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "Reset query",
+                resetButtonAriaLabel: "Reset query",
+                cancelButtonText: "Cancel",
+                cancelButtonAriaLabel: "Cancel",
+              },
+              startScreen: {
+                recentSearchesTitle: "Recent searches",
+                noRecentSearchesText: "No recent searches",
+                saveRecentSearchButtonTitle: "Save to recent searches",
+                removeRecentSearchButtonTitle: "Remove from recent searches",
+                favoriteSearchesTitle: "Favorites",
+                removeFavoriteSearchButtonTitle: "Remove from favorites",
+              },
+              errorScreen: {
+                titleText: "Unable to get results",
+                helpText: "You might need to check your network connection",
+              },
+              footer: {
+                selectText: "Select",
+                navigateText: "Navigate",
+                closeText: "Close",
+                searchByText: "Search by",
+              },
+              noResultsScreen: {
+                noResultsText: "No results found",
+                suggestedQueryText: "You can try searching for",
+                reportMissingResultsText:
+                  "Do you think this query should have results?",
+                reportMissingResultsLinkText: "Click here to report it",
+              },
+            },
+          },
+        },
+      },
+    },
+
     // all features are enabled for demo, only preserve features you need here
     mdEnhance: {
       align: true,
@@ -84,7 +177,7 @@ export default hopeTheme({
       playground: {
         presets: ["ts", "vue"],
       },
-      presentation: ["highlight", "math", "search", "notes", "zoom"],
+      revealJs: { plugins: ["highlight", "math", "search", "notes", "zoom"] },
       stylize: [
         {
           matcher: "Recommended",
@@ -103,6 +196,38 @@ export default hopeTheme({
       tabs: true,
       vPre: true,
       vuePlayground: true,
+    },
+
+    // Disable prism to use shiki
+    prismjs: false,
+
+    redirect: {
+      autoLocale: true,
+      localeConfig: {
+        "/zh/": ["zh-CN", "zh-TW", "zh"],
+        "/en/": ["en-US", "en-UK", "en"],
+      },
+    },
+
+    shiki: {
+      theme: "dark-plus",
+      langs: [
+        "ts",
+        "py",
+        "yaml",
+        "r",
+        "cpp",
+        "rust",
+        "csharp",
+        "json",
+        "makefile",
+        "ps",
+        "vue",
+        "vue-html",
+        "md",
+        "bash",
+        "diff",
+      ],
     },
   },
 });
